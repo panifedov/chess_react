6cd61c8ceb376cbef9ec840e7a486867e757b4a1
ivan.panifedov <ivan.panifedov@gmail.com>
2023-03-21 08:03:57 -0400

    Components/ modals

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BoardComponent = () => {
+    return (
+        <div
+            className='board'
+        >
+
+        </div>
+    );
+};
+
+export default BoardComponent;
